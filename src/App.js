33190afad14e441507454d394a69c033f30e6ce9
Shubham Patil <shup30@gmail.com>
2021-02-04 15:54:29 +0530

    removed router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./home";
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
